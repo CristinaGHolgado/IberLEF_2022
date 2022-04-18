9ab0b3fd54d6e9f0f5a3be343421e22f2ac35b4c
CristinaGHolgado <cristina.garcia-holgado@etu.unistra.fr>
2022-04-18 12:14:02 +0200

    lstm model

--- scripts/DL/model.py
+++ scripts/DL/model.py
@@ -9,6 +9,8 @@ Created on Sun Apr 10 21:43:43 2022
 import torch
 import numpy as np
 from torch import nn
+from torch.nn.utils.rnn import pack_padded_sequence
+import torch.nn.functional as F
 from transformers import AutoTokenizer, AutoModel
 
 
@@ -36,3 +38,26 @@ class BertClassifier(nn.Module):
         final_layer = self.relu(linear_output)
 
         return final_layer
+    
+class LSTM_Model(nn.Module):
+    def __init__(self, lm, nclass):
+        super().__init__()
+        
+        self.bert = AutoModel.from_pretrained(lm)
+        self.hidden_size = self.bert.config.hidden_size
+        self.LSTM = nn.LSTM(input_size= self.hidden_size, hidden_size=self.hidden_size, bidirectional=True)
+        self.clf = nn.Linear(self.hidden_size*2, nclass)
+        
+    def forward(self, input_ids, masks):
+        with torch.no_grad():
+            #encoded_layers, pooled_
+            output = self.bert(input_ids=input_ids, attention_mask= masks)
+            encoded_layers, pooled_output = output[0], output[1]
+        #print(output)
+        encoded_layers = encoded_layers.permute(1, 0, 2)
+        enc_hiddens, (last_hidden, last_cell) = self.LSTM(pack_padded_sequence(encoded_layers, masks))
+        output_hidden = torch.cat((last_hidden[0], last_hidden[1]), dim=1)
+        output_hidden = F.dropout(output_hidden, 0.2)
+        output = self.clf(output_hidden)
+        
+        return F.sigmoid(output)
